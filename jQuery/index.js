9da2a6c502ee2541c55c9382c91b3fd62826a736
HARISH SEMWAL <96759566+Harishsemwal@users.noreply.github.com>
2024-04-17 13:46:59 +0530

    Complete starting to end in shortcut
    Complete start to end in shortcut with Tips and Tricks

---
+++ jQuery/index.js
@@ -0,0 +1,172 @@
+// // $("h1").css("color", "blue");
+
+// $("h1").addClass("demo");
+
+// $("h1").click(function(){
+//     $(this).text("I was clicked!");
+// })
+
+
+
+
+
+
+
+// // add add the html in Using jQuery to the html file
+
+// $("h1").html("<em>Hey</em>");
+
+// $("a").attr("href", "https://www.yahoo.com");
+
+
+
+
+
+
+
+
+// Event Listener in jQuery------------:
+
+// ----------This is Using JavaScipt--------------------
+
+// for(var i = 0; i < 5; i++){
+//     document.querySelectorAll("button")[i].addEventListener("click", function(){
+//         document.querySelector("h1").style.color = "purple";
+//     });
+// }   
+
+
+
+
+
+
+// --------------This is Using jQuery--------------------
+// for(var i = 0; i < 5; i++){
+//     $("button").click(function(){
+//         $("h1").css("color", "purple");
+//     });
+// }
+
+
+
+
+
+
+// $("button").click(function(){
+//     $("h1").css.color("purple");
+
+// });
+
+
+
+
+
+
+// $("h1").click(function(){
+//     $(this).css("color", "purple");
+// });
+
+
+
+
+
+// $("input").keypress(function(event){
+//     console.log(event.key);
+// })
+
+
+
+
+
+
+//change the charcter whenever use the keypress event
+// $("body").keypress(function(event){
+//     $("h1").text(event.key);
+// });
+
+
+
+
+// ------------------------Mouse Over Function----------------------------
+// $("h1").on("mouseover", function(){
+//     $("h1").css("color", "purple");
+// });
+
+
+
+
+
+// -------------adding and removing element in jQuery------------------- 
+// $("h1").before("<button>New</button>");
+// $("h1").after("<button>New</button>");
+
+// $("h1").prepend("<button>New</button>");
+// $("h1").append("<button>New</button>");
+
+// $("button").remove();
+
+
+
+
+
+
+
+// ---------------------Click The Button it will be hide------------
+
+// $("button").on("click", function(){
+//     $("h1").hide();
+// });      //hide the h1 tag
+
+
+// $("button").on("click", function(){
+//     $("h1").toggle();
+// });      //toggle the h1 tag        
+
+// $("button").on("click", function(){
+//     $("h1").fadeOut();
+// });      //fadeout the h1 tag
+
+// $("button").on("click", function(){
+//     $("h1").fadeIn();
+// });      //fadein the h1 tag
+
+// $("button").on("click", function(){
+//     $("h1").fadeToggle();
+
+// });      //fadeToggle the h1 tag
+
+// $("button").on("click", function(){
+//     $("h1").slideUp();
+// });      //slideUp the h1 tag
+
+// $("button").on("click", function(){
+//     $("h1").slideDown();
+// });      //slideDown the h1 tag
+
+// $("button").on("click", function(){
+//     $("h1").slideToggle();
+
+// });      //slideToggle the h1 tag
+
+// $("button").on("click", function(){
+//     $("h1").animate({opacity: 0.5});
+
+// });      //animate the h1 tag
+
+// $("button").on("click", function(){
+//     $("h1").animate({margin: "20%"});
+
+// });      //animate the h1 tag
+
+// $("button").on("click", function(){
+//     $("h1").animate({margin: "20%"});
+
+// });      //animate the h1 tag
+
+// $("button").on("click", function(){
+//     $("h1").slideUp().slideDown().animate({opacity: 0.5});
+
+// });      //animate the h1 tag
+
+//-------------------Tips for jQuery from Angla Yu ---------------------
+
